31110fd6a1af4a37a3bdf528456ad24344840026
Kishan <Kishan.kumar@meritech.co.in>
2021-07-02 14:49:08 +0530

    added datamodel project

---
+++ DataModel/ConnectionStringPath.cs
@@ -0,0 +1,15 @@
+﻿namespace DataModel
+{
+    public class ConnectionStringPath
+    {
+        /// <summary>
+        /// SQL connection from web config file.
+        /// </summary>
+        public static string Connection { get; set; }
+
+        /// <summary>
+        /// SQL connection to master database from web config file.
+        /// </summary>
+        public static string MasterConnection { get; set; }
+    }
+}
---
+++ DataModel/ShopBridge.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModel
+{
+    public class ShopBridge
+    {
+        public int Id { get; set; }
+
+        [StringLength(100)]
+        public string Name { get; set; }
+
+
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        public double Price { get; set; }
+
+        public DateTime AddedDateTime { get; set; }
+    }
+}
