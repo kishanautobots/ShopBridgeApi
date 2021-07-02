569f25287953b3029f5f560f92edb79b641f6f03
Kishan <Kishan.kumar@meritech.co.in>
2021-07-02 14:51:15 +0530

    Added data model sql project

---
+++ DataModelSQL/ShopBridgeConfigurationDBManager.cs
@@ -0,0 +1,169 @@
+﻿using Centra.DataModelInfrastructure;
+using DataModel;
+using DataModelSql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModelSQL
+{
+    public class ShopBridgeConfigurationDBManager : IShopBridgeConfiguration
+    {
+        /// <summary>
+        /// Save product detail
+        /// </summary>
+        /// <param name="productConfiguration"></param>
+        /// <returns></returns>
+        public string SaveProductConfiguration(ShopBridge productConfiguration)
+        {
+            string status = "Error";
+            if (productConfiguration == null)
+            {
+                throw new ArgumentNullException("ShopBridge detail null");
+            }
+
+            string query = string.Format(@"Insert into dbo.shop_bridge_products (name, description, price, added_date_time) values('{0}', '{1}', {2}, GetUtcDate())"
+                                , productConfiguration.Name, productConfiguration.Description, productConfiguration.Price);
+
+            try
+            {
+                SqlHelper.Instance.ExecuteNonQuery(query);
+                status = "Success";
+            }
+            catch (Exception ex)
+            {
+                status = "Error";
+            }
+
+            return status;
+        }
+
+        /// <summary>
+        /// Update product detail
+        /// </summary>
+        /// <param name="productConfiguration"></param>
+        /// <returns></returns>
+        public string UpdateProductConfiguration(ShopBridge productConfiguration)
+        {
+            string status = "Error";
+            if (productConfiguration == null)
+            {
+                throw new ArgumentNullException("ShopBridge detail null");
+            }
+
+            string query = string.Format(@"Update dbo.shop_bridge_products set name = '{0}', description='{1}', price={2}, modified_date_time= GetUtcDate() where
+                                            id = {3}", productConfiguration.Name, productConfiguration.Description, productConfiguration.Price, productConfiguration.Id);
+            try
+            {
+                SqlHelper.Instance.ExecuteNonQuery(query);
+                status = "Success";
+            }
+            catch (Exception ex)
+            {
+                status = "Error";
+            }
+            return status;
+        }
+
+        /// <summary>
+        /// Delete product
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public string DeleteProduct(int id)
+        {
+            string status = "Error";
+            try
+            {
+                SqlHelper.Instance.ExecuteNonQuery("delete from dbo.shop_bridge_products where id = " + id);
+                status = "Success";
+            }
+            catch (Exception ex)
+            {
+                return "Error";
+            }
+
+            return status;
+        }
+
+        /// <summary>
+        /// get all product
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public List<ShopBridge> GetproductList()
+        {
+            List<ShopBridge> productList = null;
+            try
+            {
+                using (SqlDataReader reader = (SqlDataReader)SqlHelper.Instance.ExecuteDataReader("select * from dbo.shop_bridge_products", CommandType.Text))
+                {
+                    while (reader.Read())
+                    {
+                        if (reader.HasRows)
+                        {
+                            if (productList == null)
+                                productList = new List<ShopBridge>();
+
+                            productList.Add(new ShopBridge
+                            {
+                                Id = reader.GetInt32(0),
+                                Name = reader.GetString(1),
+                                Description = reader.GetString(2),
+                                Price = reader.GetDouble(3),
+                                AddedDateTime = reader.GetDateTime(4)
+                            });
+
+                        }
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return productList;
+            }
+
+            return productList;
+        }
+
+
+        public ShopBridge Getproduct(string name)
+        {
+            ShopBridge productList = null;
+            try
+            {
+                using (SqlDataReader reader = (SqlDataReader)SqlHelper.Instance.ExecuteDataReader("select * from dbo.shop_bridge_products where name = " + name, CommandType.Text))
+                {
+                    while (reader.Read())
+                    {
+                        if (reader.HasRows)
+                        {
+                            if (productList == null)
+                                productList = new ShopBridge()
+                                {
+                                    Id = reader.GetInt32(0),
+                                    Name = reader.GetString(1),
+                                    Description = reader.GetString(2),
+                                    Price = reader.GetDouble(3),
+                                    AddedDateTime = reader.GetDateTime(4)
+                                };
+
+                        }
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return productList;
+            }
+
+            return productList;
+        }
+    }
+}
---
+++ DataModelSQL/SqlHelper.cs
@@ -0,0 +1,193 @@
+﻿
+using DataModel;
+using DataModelInfrastructure;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace DataModelSql
+{
+    /// <summary>This class will have all sql functions used view, add, update, delete data.</summary>
+    public class SqlHelper : IDblHelper
+    {
+        private const int _DefaultCommandTimeout = 2400;
+        private static SqlHelper _instance;
+        private static readonly object _instanceSyncLock = new object();
+        private string _connectionString;
+
+        public SqlHelper(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        /// <summary>The <see cref="Core"/> singleton instance.</summary>
+        public static SqlHelper Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_instanceSyncLock)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new SqlHelper(ConnectionStringPath.Connection);
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        /// <summary>
+        /// This will return data reader based on the query.
+        /// </summary>
+        /// <remarks>It is the responsibility of the caller to clean up the returned DataReader.</remarks>
+        /// <param name="commandText">The text to set as the CommandText.</param>
+        /// <param name="parameters">The <see cref="IDataParameter"/>s to give to the SQL.</param>
+        /// <returns>An <see cref="SqlDataReader"/>.</returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Reliability", "CA2000:Dispose objects before losing scope")]
+        public IDataReader ExecuteDataReader(string commandText, IEnumerable<IDataParameter> commandParameters = null)
+        {
+            return ExecuteDataReader(commandText, CommandType.Text, commandParameters);
+        }
+
+        /// <summary>
+        /// This will return data reader based on the query.
+        /// </summary>
+        /// <remarks>It is the responsibility of the caller to clean up the returned DataReader.</remarks>
+        /// <param name="commandText">The text to set as the CommandText.</param>
+        /// <param name="commandType"></param>
+        /// <param name="parameters">The <see cref="IDataParameter"/>s to give to the SQL.</param>
+        /// <returns>An <see cref="SqlDataReader"/>.</returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Reliability", "CA2000:Dispose objects before losing scope")]
+        public IDataReader ExecuteDataReader(
+            string commandText,
+            CommandType commandType,
+            IEnumerable<IDataParameter> commandParameters = null
+        )
+        {
+            // NOTE: No usings because we are returning the DataReader.  When the data reader closes, it will close the
+            // command and connection.
+            SqlConnection connection = CreateAndOpenConnection(_connectionString);
+            SqlCommand command = CreateCommand(connection, commandText, commandType, commandParameters);
+
+            // When returning datareader the connection need to be open, so by using command behaviour
+            // "CommandBehavior.CloseConnection" this ensures that when datareader is used closed by the using
+            // function the connection will automatically gets closed.
+            return command.ExecuteReader(CommandBehavior.CloseConnection);
+        }
+
+        /// <summary>This will execute a query in the database.</summary>
+        /// <param name="commandText">The query to execute</param>
+        /// <returns>The number of rows affected</returns>
+        public int ExecuteNonQuery(string commandText)
+        {
+            return ExecuteNonQuery(commandText, CommandType.Text, null);
+        }
+        
+
+        /// <summary>This will execute a query in the database.</summary>
+        /// <param name="commandText">The query to execute</param>
+        /// <param name="commandType">The <see cref="CommandType"/>.</param>
+        /// <param name="commandParameters">The optional <see cref="IDataParameter"/>s.</param>
+        /// <returns>The number of rows affected</returns>
+        public int ExecuteNonQuery(
+            string commandText,
+            CommandType commandType,
+            IEnumerable<IDataParameter> commandParameters
+        )
+        {
+            using (var connection = CreateAndOpenConnection(_connectionString))
+            {
+                using (var command = CreateCommand(connection, commandText, commandType, commandParameters))
+                {
+                    return command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        /// <summary>Creates AND opens a new <see cref="SqlConnection"/> with the given connection string.</summary>
+        /// <param name="connectionString">Connection string to use when connecting to DB.</param>
+        /// <returns>An open <see cref="SqlConnection"/>.</returns>
+        /// <remarks><see cref="SqlConnection"/> is <see cref="IDisposable"/>.  Callers of this function must be
+        /// careful to dispose the returned connection properly.</remarks>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Reliability", "CA2000:Dispose objects before losing scope")]
+        public SqlConnection CreateAndOpenConnection()
+        {
+            return CreateAndOpenConnection(_connectionString);
+        }
+
+        /// <summary>
+        /// Returns connection string.
+        /// </summary>
+        /// <returns></returns>
+        public string GetConnectionString()
+        {
+            return _connectionString;
+        }
+
+        /// <summary>Creates AND opens a new <see cref="SqlConnection"/> with the given connection string.</summary>
+        /// <param name="connectionString">Connection string to use when connecting to DB.</param>
+        /// <returns>An open <see cref="SqlConnection"/>.</returns>
+        /// <remarks><see cref="SqlConnection"/> is <see cref="IDisposable"/>.  Callers of this function must be
+        /// careful to dispose the returned connection properly.</remarks>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Reliability", "CA2000:Dispose objects before losing scope")]
+        public static SqlConnection CreateAndOpenConnection(string connectionString)
+        {
+            var connection = new SqlConnection(connectionString);
+
+            connection.Open();
+            return connection;
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="SqlCommand"/> with default options.  The default options are hard-coded.
+        /// For now, the only option is the timeout, which is set to infinite.
+        /// </summary>
+        /// <param name="connection">The <see cref="SqlConnection"/> to create the command on.</param>
+        /// <returns>An <see cref="SqlCommand"/>.</returns>
+        public static SqlCommand CreateDefaultCommand(SqlConnection connection)
+        {
+            if (connection == null)
+            {
+                throw new ArgumentNullException("connection");
+            }
+
+            var command = connection.CreateCommand();
+            command.CommandTimeout = _DefaultCommandTimeout;
+            return command;
+        }
+
+        /// <summary>Creates a <see cref="SqlCommand"/></summary>
+        /// <remarks>NOTE: SqlComand is IDisposable</remarks>
+        public static SqlCommand CreateCommand(SqlConnection connection, string text)
+        {
+            return CreateCommand(connection, text, CommandType.Text, null);
+        }
+
+        /// <summary>
+        /// Creates a <see cref="SqlCommand"/>
+        /// </summary>
+        /// <remarks>NOTE: SqlComand is IDisposable</remarks>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Security", "CA2100:Review SQL queries for security vulnerabilities"), System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Reliability", "CA2000:Dispose objects before losing scope")]
+        public static SqlCommand CreateCommand(
+            SqlConnection connection,
+            string text,
+            CommandType type,
+            IEnumerable<IDataParameter> commandParameters = null
+        )
+        {
+            var command = CreateDefaultCommand(connection);
+            command.CommandType = type;
+            command.CommandText = text;
+            if (commandParameters != null)
+            {
+                command.Parameters.AddRange(commandParameters.ToArray());
+            }
+            return command;
+        }
+    }
+}
