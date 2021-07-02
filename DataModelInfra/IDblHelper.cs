5350e77a9843d132d3c74e11ac89d6f7ad38d6f8
Kishan <Kishan.kumar@meritech.co.in>
2021-07-02 14:50:18 +0530

    Added data model infrastructure project

---
+++ DataModelInfra/IDblHelper.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Data;
+
+namespace DataModelInfrastructure
+{
+    public interface IDblHelper
+    {
+        IDataReader ExecuteDataReader(string commandText, CommandType commandType, IEnumerable<IDataParameter> commandParameters = null);
+
+        int ExecuteNonQuery(string commandText);
+
+        string GetConnectionString();
+    }
+}
---
+++ DataModelInfra/IShopBridgeConfiguration.cs
@@ -0,0 +1,22 @@
+﻿
+using DataModel;
+using System;
+using System.Collections.Generic;
+
+namespace Centra.DataModelInfrastructure
+{
+    public interface IShopBridgeConfiguration
+    {
+        string SaveProductConfiguration(ShopBridge productConfiguration);
+
+        string DeleteProduct(int id);
+
+        string UpdateProductConfiguration(ShopBridge productConfiguration);
+
+        List<ShopBridge> GetproductList();
+
+        ShopBridge Getproduct(string name);
+
+
+    }
+}
