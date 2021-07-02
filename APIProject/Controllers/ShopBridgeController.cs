0851169d0ad5cf13a163faeca5396d548fda9927
Kishan <Kishan.kumar@meritech.co.in>
2021-07-02 14:45:18 +0530

    added files

---
+++ APIProject/Controllers/ShopBridgeController.cs
@@ -0,0 +1,53 @@
+﻿using APIProject.Filter;
+using Centra.Core;
+using Centra.DataModelInfrastructure;
+using DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace APIProject.Controllers
+{
+    [CustomExceptionFilter]
+    public class ShopBridgeController : ApiController
+    {
+        [Route("ShopBridge/GetAllProduct")]
+        [HttpGet]
+        public IEnumerable<ShopBridge> GetAllProduct()
+        {
+            return ShopBridgeConfigurationManager.Instance.GetAllProdcuts();
+        }
+
+        [Route("ShopBridge/AddProduct")]
+        [HttpPost]
+        public string AddProduct([FromBody]ShopBridge product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return "One of the property value wrong or exceeded.";
+            }
+            return ShopBridgeConfigurationManager.Instance.AddNewProduct(product);
+        }
+
+        [Route("ShopBridge/UpdateProduct")]
+        [HttpPost]
+        public string UpdateProduct([FromBody]ShopBridge product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return "One of the property value wrong or exceeded.";
+            }
+            return ShopBridgeConfigurationManager.Instance.UpdateProduct(product);
+        }
+
+        [Route("ShopBridge/DeleteProduct/{id}")]
+        [HttpDelete]
+        public string DeleteProduct(int id)
+        {
+            return ShopBridgeConfigurationManager.Instance.DeleteProduct(id);
+        }
+    }
+}
---
+++ APIProject/Filter/CustomExceptionFilter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http.Filters;
+
+namespace APIProject.Filter
+{
+    public class CustomExceptionFilter : ExceptionFilterAttribute
+    {
+        public override void OnException(HttpActionExecutedContext actionExecutedContext)
+        {
+            string exceptionMessage = string.Empty;
+
+            if (actionExecutedContext.Exception.InnerException == null)
+            {
+                exceptionMessage = actionExecutedContext.Exception.Message;
+            }
+            else
+            {
+                exceptionMessage = actionExecutedContext.Exception.InnerException.Message;
+            }
+
+            var response = new HttpResponseMessage(HttpStatusCode.InternalServerError)
+            {
+                Content = new StringContent("An unhandled exception was thrown by service."),
+                    ReasonPhrase = "Internal Server Error."
+            };
+            actionExecutedContext.Response = response;
+        }
+    }
+}
