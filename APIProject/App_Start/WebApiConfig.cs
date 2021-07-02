7a55ee3a987a05115ce6dc37501234755dbce010
Kishan <Kishan.kumar@meritech.co.in>
2021-07-02 14:39:19 +0530

    Added files

---
+++ APIProject/App_Start/WebApiConfig.cs
@@ -0,0 +1,26 @@
+﻿using APIProject.Filter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace APIProject
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            config.Filters.Add(new CustomExceptionFilter());
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
