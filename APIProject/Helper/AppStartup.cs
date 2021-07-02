577dbd118f072794115cbfe622d9707e5ca0878a
Kishan <Kishan.kumar@meritech.co.in>
2021-07-02 14:44:21 +0530

    Added files

---
+++ APIProject/Helper/AppStartup.cs
@@ -0,0 +1,16 @@
+﻿
+using DataModel;
+
+namespace Api.Helpers
+{
+    public class AppStartup
+    {
+        /// <summary>
+        /// Loads this instance.
+        /// </summary>
+        public static void Load()
+        {
+            ConnectionStringPath.Connection = System.Configuration.ConfigurationManager.ConnectionStrings["SqlConnString"].ConnectionString;
+        }
+    }
+}
